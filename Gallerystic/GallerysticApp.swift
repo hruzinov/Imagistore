87e94cdb567f34453a098cd34813c9ddd218a160
Evhen Gruzinov <evhen@gruzinov.com>
2023-03-12 19:04:22 +0300

    Initial Commit

---
+++ Gallerystic/GallerysticApp.swift
@@ -0,0 +1,17 @@
+//
+//  GallerysticApp.swift
+//  Gallerystic
+//
+//  Created by Evhen Gruzinov on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct GallerysticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
